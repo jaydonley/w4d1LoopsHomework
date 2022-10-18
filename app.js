a03f0f4e3dfa87fec83123cad94ef1f46290aeba
Jason Donley <jay.donley@gmail.com>
2022-10-18 00:56:41 -0400

    w4d1LoopsHomework

---
+++ app.js
@@ -0,0 +1,35 @@
+let arrayAlpha = [];
+
+for (let i = 1; i <= 10; i++) {
+  arrayAlpha.push(i);
+}
+console.log(arrayAlpha);
+
+//------------------------------------
+
+let arrayBravo = [];
+let i = 1;
+
+while (i <= 10) {
+  arrayBravo.push(i);
+  i++;
+}
+console.log(arrayBravo);
+
+//---------------------------------------------
+
+let arrayCharlie = [];
+
+for (let j = 11; j <= 21; j+=2){
+    arrayCharlie.push(j);
+}
+console.log(arrayCharlie);
+
+//--------------------------------------------
+
+let arrayDelta = [];
+
+for (let d = 15; d >= 1; d-=2){
+    arrayDelta.push(d);
+}
+console.log(arrayDelta);
